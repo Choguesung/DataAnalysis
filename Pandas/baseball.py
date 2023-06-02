940457e3830c01f8433f8752295e825dbb4adb01
LSH <121619411+Choguesung@users.noreply.github.com>
2023-06-02 17:10:23 +0900

    Update baseball.py

--- Pandas/baseball.py
+++ Pandas/baseball.py
@@ -10,7 +10,7 @@ df = pd.read_csv('baseball.csv')
 X = df[['teamA_war1', 'teamA_war2', 'teamA_war3', 'teamA_war4', 'teamA_war5', 'teamA_war6', 'teamA_war7', 'teamA_war8', 'teamA_war9', 'teamA_war10', 'teamB_war1', 'teamB_war2', 'teamB_war3', 'teamB_war4', 'teamB_war5', 'teamB_war6', 'teamB_war7', 'teamB_war8', 'teamB_war9', 'teamB_war10']]
 y = df['teamA_win']
 
-# 학습용 데이터와 검증용 데이터를 나눕니다.1
+# 학습용 데이터와 검증용 데이터를 나눕니다.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # 로지스틱 회귀 모델을 생성하고 학습합니다.
