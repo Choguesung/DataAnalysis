15dc7079830c9e2fc8bf6037ac75eba4597658be
LSH <121619411+Choguesung@users.noreply.github.com>
2023-09-24 20:26:14 +0900

    1

---
+++ Tensorflow/clothes.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+(trainX, trainY), (testX, testY) = tf.keras.datasets.fashion_mnist.load_data()
+
+trainX = trainX / 255.0
+testX = testX / 255.0
+
+trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+testX = testX.reshape((testX.shape[0], 28, 28, 1))
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), padding="same", activation='relu', input_shape=(28, 28, 1)), 
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax'),
+])
+
+model.summary()
+
+model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
+model.fit(trainX, trainY, validation_data=(testX,testY), epochs=5)
+
+score = model.evaluate(testX, testY) # 모델 평가 
+print(score)
