dddb0e5913a08158febe0b1715563f2809738433
LSH <121619411+Choguesung@users.noreply.github.com>
2023-09-24 14:24:37 +0900

    Update admit.py

--- Tensorflow/admit.py
+++ Tensorflow/admit.py
@@ -1,15 +1,16 @@
 import tensorflow as tf
 import pandas as pd
+import numpy as np
 
 data = pd.read_csv('gpascore.csv')
 
 data = data.dropna()
 
 y데이터 = data['admit'].values
-x데이터 = []
+x데이터 = [ ]
 
 for i,rows in data.iterrows():
-    x데이터.append([(rows['gre']),rows['gpa'],rows['rank']])
+    x데이터.append([rows['gre'],rows['gpa'],rows['rank']])
 
 model = tf.keras.models.Sequential([ 
     tf.keras.layers.Dense(64, activation='tanh'),
@@ -17,7 +18,10 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(1, activation='sigmoid'),
  ])
 
-model.compile(optimizer='adam',loss='binary_crossmentropy',metrics=['accuracy'])
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
 
-model.fit(x데이터,y데이터, epochs=1)
+model.fit(np.array(x데이터),np.array(y데이터), epochs=1000)
 
+예측값 = model.predict( [ [750,3.70,3],[400,2.2,2]])
+
+print(예측값)
